b9df9f9a0a5b951115d0b1b1c0f1b75ec4315513
Amar Al Farizi <akunsaya3123@gmail.com>
2025-10-25 21:18:50 +0700

    create structure folder

---
+++ template.sh
@@ -0,0 +1,15 @@
+#Create Directory Structure
+mkdir -p src
+mkdir -p resources
+
+#Create Files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.py
+touch requirements.txt
+
+echo "Project structure created successfully."
